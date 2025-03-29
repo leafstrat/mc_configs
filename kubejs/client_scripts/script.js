1a1f76885b64732ce54386ad94dc549b422dec62
Justin <leafstrat@gmail.com>
2025-03-29 05:19:39 -0700

    Update script.js

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -1,6 +1,6 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time client resources reload)')
+console.info('Hello, Worlds! (You will see this line every time client resources reload)')
 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
